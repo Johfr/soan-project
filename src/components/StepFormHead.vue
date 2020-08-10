b71b83b60b00ee69140cd09495324e3c56952d9e
Joh <johanne.assorin@hotmail.com>
2020-08-10 17:05:09 +0200

    soan003 : form create html, css + form steps js

---
+++ src/components/StepFormHead.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="step_form-head">
+    <div class="step one active">
+      <div class="step-content">
+        <div class="step-icon">
+          <svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="282.4 326.5 15 16.9" enable-background="new 282.4 326.5 15 16.9" xml:space="preserve">
+              <g>
+                <path d="M296.2,330.8l1.2-1.3v0h0c-0.1-0.8-0.5-1.6-1.2-2.1c-0.7-0.5-1.5-0.8-2.2-0.8l0,0l0,0l-1.1,1.3c0.8,0.1,1.5,0.4,2.2,0.9
+                  C295.6,329.4,296,330.1,296.2,330.8z M286,339.2c-0.7-0.6-1.5-0.8-2.3-0.8l0,0l-0.7,2.5l-0.7,2.5l2.4-1l2.4-1l0,0
+                  C287,340.6,286.7,339.8,286,339.2z M293.2,334.3l2.7-3.1v0h0c-0.1-0.8-0.5-1.6-1.2-2.1c-0.7-0.5-1.5-0.8-2.2-0.8l0,0v0l-2.7,3.1
+                  l0,0l0,0l-0.6,0.7l-5.2,6c0.8,0.1,1.5,0.4,2.2,0.9c0.7,0.5,1.1,1.3,1.3,2l5.2-6L293.2,334.3L293.2,334.3L293.2,334.3z"/>
+              </g>
+          </svg>
+        </div>
+        <p class="step-label">
+          Statut
+        </p>
+      </div>
+    </div>
+
+    <div class="step two hidden">
+      <div class="step-content">
+        <div class="step-icon">
+          <svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="282.4 326.5 15 16.9" enable-background="new 282.4 326.5 15 16.9" xml:space="preserve">
+              <g>
+                <path d="M296.2,330.8l1.2-1.3v0h0c-0.1-0.8-0.5-1.6-1.2-2.1c-0.7-0.5-1.5-0.8-2.2-0.8l0,0l0,0l-1.1,1.3c0.8,0.1,1.5,0.4,2.2,0.9
+                  C295.6,329.4,296,330.1,296.2,330.8z M286,339.2c-0.7-0.6-1.5-0.8-2.3-0.8l0,0l-0.7,2.5l-0.7,2.5l2.4-1l2.4-1l0,0
+                  C287,340.6,286.7,339.8,286,339.2z M293.2,334.3l2.7-3.1v0h0c-0.1-0.8-0.5-1.6-1.2-2.1c-0.7-0.5-1.5-0.8-2.2-0.8l0,0v0l-2.7,3.1
+                  l0,0l0,0l-0.6,0.7l-5.2,6c0.8,0.1,1.5,0.4,2.2,0.9c0.7,0.5,1.1,1.3,1.3,2l5.2-6L293.2,334.3L293.2,334.3L293.2,334.3z"/>
+              </g>
+          </svg>
+        </div>
+        <p class="step-label">
+          Information
+        </p>
+      </div>
+    </div>
+
+    <div class="step three hidden">
+      <div class="step-content">
+        <div class="step-icon">
+          <svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="282.4 326.5 15 16.9" enable-background="new 282.4 326.5 15 16.9" xml:space="preserve">
+              <g>
+                <path d="M296.2,330.8l1.2-1.3v0h0c-0.1-0.8-0.5-1.6-1.2-2.1c-0.7-0.5-1.5-0.8-2.2-0.8l0,0l0,0l-1.1,1.3c0.8,0.1,1.5,0.4,2.2,0.9
+                  C295.6,329.4,296,330.1,296.2,330.8z M286,339.2c-0.7-0.6-1.5-0.8-2.3-0.8l0,0l-0.7,2.5l-0.7,2.5l2.4-1l2.4-1l0,0
+                  C287,340.6,286.7,339.8,286,339.2z M293.2,334.3l2.7-3.1v0h0c-0.1-0.8-0.5-1.6-1.2-2.1c-0.7-0.5-1.5-0.8-2.2-0.8l0,0v0l-2.7,3.1
+                  l0,0l0,0l-0.6,0.7l-5.2,6c0.8,0.1,1.5,0.4,2.2,0.9c0.7,0.5,1.1,1.3,1.3,2l5.2-6L293.2,334.3L293.2,334.3L293.2,334.3z"/>
+              </g>
+          </svg>
+        </div>
+        <p class="step-label">
+          Représentant
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StepFormHead',
+  data () {
+    return {
+      steps : null
+    }
+  },
+  props: {
+    activeStep: Number
+  },
+  watch: {
+    activeStep : function () {
+      console.log(this.steps)
+      
+
+      this.steps.forEach((step, index) => {
+        if(index != this.activeStep) {
+          this.steps[index].classList.remove('active')
+          this.steps[index].classList.add('hidden')
+        } else {
+          this.steps[this.activeStep].classList.add('active')
+          this.steps[this.activeStep].classList.remove('hidden')
+        }
+        // step.classList.remove('active')
+      })
+
+    }
+  },
+  mounted () {
+    this.steps = document.querySelectorAll('.step')
+    
+      console.log(this.steps)
+    // console.log(this.activeStep)
+  },
+}
+</script>
+
+<style scoped lang="scss">
+$blue-link : #0175fb;
+
+.step_form-head {
+  display: flex;
+  justify-content: space-between;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+
+.step {
+  display: flex;
+
+  &.active {
+    .step-icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 30px;
+      height: 30px;
+      margin: 0;
+      border-color: $blue-link;
+    }
+    svg {
+      display: inline-block;
+      fill: $blue-link;
+      width: 15px;
+    }
+    .step-label {
+      color: $blue-link;
+    }
+  }
+
+  &.hidden {
+    svg {
+      display: none;
+    }
+  }
+
+  .step-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .step-icon {
+    width: 5px;
+    height: 5px;
+    margin: 15px;
+    border: 1px solid #ccc;
+    border-radius: 50%;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,352 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <div class="form-container">
+        <!-- Import des étapes -->
+        <StepFormHead v-bind:activeStep="indexActive"/>
+
+        <!-- Import du formulaire -->
+
+        <form action="" method="POST">
+
+          <div class="etape etape1 active">
+            <p>créer une entreprise</p>
+            <h2>Quel est le statut de votre entreprise ?</h2>
+            <select name="" id="">
+              <option value="">EURL</option>
+              <option value="">SARL</option>
+            </select>
+            
+            <div class="btn-container">
+              <p class="btn-previous">revenir plus tard</p>
+              <p class="btn-next" @click="checkStep">continuer</p>
+            </div>
+          </div>
+          
+          <!-- Etape 2 -->
+          <div class="etape etape2 hidden">
+            <p>créer une entreprise</p>
+            <h2>Informations de l'entreprise</h2>
+
+            <!-- Nom légal de l'entreprise -->
+            <label for="nom_entreprise">
+              Nom légal de l'entreprise*
+            </label>
+            <input type="text" placeholder="freelance" id="nom_entreprise" required>
+            
+            <!-- Email -->
+            <label for="email_entreprise">
+              Email de l'entreprise*
+            </label>
+            <input type="text" placeholder="email" id="email_entreprise" required>
+            
+            <!-- Adresse -->
+            <label for="adresse_entreprise">
+              Adresse de l'entreprise*
+            </label>
+            <input type="text" placeholder="freelance" id="adresse_entreprise" required>
+            
+            <!-- Etape double -->
+            <div class="etape-double">
+              <div class="block1">
+                <label for="code_postal">
+                  Code postal*
+                </label>
+                <input type="number" placeholder="freelance" id="code_postal" required>
+              </div>
+
+              <div class="block2">
+                <label for="ville">
+                  Ville*
+                </label>
+                <input type="number" placeholder="freelance" id="ville" required>
+              </div>
+            </div>
+
+            <!-- region -->
+            <label for="adresse_entreprise">
+              Adresse de l'entreprise*
+            </label>
+            <input type="text" placeholder="freelance" id="adresse_entreprise" required>
+
+            <!-- pays -->
+            <label for="adresse_entreprise">
+              Pays*
+            </label>
+            <input type="text" placeholder="freelance" id="adresse_entreprise" required>
+
+            <!-- Siret -->
+            <label for="adresse_entreprise">
+              Siret*
+            </label>
+            <input type="text" placeholder="freelance" id="adresse_entreprise" required>
+
+            <!-- Capital -->
+            <label for="adresse_entreprise">
+              Capital social de l'entreprise*
+            </label>
+            <input type="text" placeholder="freelance" id="adresse_entreprise" required>
+            
+            <p class="required-form-info">*Informations obligatoires</p>
+
+            <!-- btn validation  -->
+            <div class="btn-container">
+              <p class="btn-previous" @click="previousStep">revenir plus tard</p>
+              <p class="btn-next" @click="checkStep">continuer</p>
+            </div>
+          </div>
+
+          <!-- Etape 3 -->
+          <div class="etape etape3 hidden">
+            <p>créer une entreprise</p>
+            <h2>Représentant de l'entreprise</h2>
+
+            <!-- prenom du représentant -->
+            <label for="prenom_representant">
+              Prénom du représentant*
+            </label>
+            <input type="text" placeholder="freelance" id="prenom_representant" required>
+
+            <!-- Nom du représentant -->
+            <label for="nom_representant">
+              Nom du représentant*
+            </label>
+            <input type="text" placeholder="freelance" id="nom_representant" required>
+            
+            <!-- Email -->
+            <label for="email_entreprise">
+              Email de l'entreprise*
+            </label>
+            <input type="text" placeholder="email" id="email_entreprise" required>
+            
+            <!-- Adresse -->
+            <label for="adresse_entreprise">
+              Adresse de l'entreprise*
+            </label>
+            <input type="text" placeholder="freelance" id="adresse_entreprise" required>
+            
+            <!-- Etape double -->
+            <div class="etape-double">
+              <div class="block1">
+                <label for="code_postal">
+                  Code postal*
+                </label>
+                <input type="number" placeholder="freelance" id="code_postal" required>
+              </div>
+
+              <div class="block2">
+                <label for="ville">
+                  Ville*
+                </label>
+                <input type="number" placeholder="freelance" id="ville" required>
+              </div>
+            </div>
+
+            <!-- region -->
+            <label for="adresse_entreprise">
+              Adresse de l'entreprise*
+            </label>
+            <input type="text" placeholder="freelance" id="adresse_entreprise" required>
+
+            <!-- pays -->
+            <label for="adresse_entreprise">
+              Pays*
+            </label>
+            <input type="text" placeholder="freelance" id="adresse_entreprise" required>
+
+            <!-- Siret -->
+            <label for="adresse_entreprise">
+              Siret*
+            </label>
+            <input type="text" placeholder="freelance" id="adresse_entreprise" required>
+
+            <!-- Capital -->
+            <label for="adresse_entreprise">
+              Capital social de l'entreprise*
+            </label>
+            <input type="text" placeholder="freelance" id="adresse_entreprise" required>
+            
+            <p class="required-form-info">*Informations obligatoires</p>
+
+            <!-- btn validation  -->
+            <div class="btn-container">
+              <p class="btn-previous" @click="previousStep">revenir plus tard</p>
+              <p class="btn-next">continuer</p>
+            </div>
+          </div>
+
+        </form>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import StepFormHead from '@/components/StepFormHead.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    StepFormHead
+  },
+  data () {
+    return {
+      isValidated : false,
+      steps: null,
+      indexActive: 0
+    }
+  },
+  mounted () {
+    this.steps = document.querySelectorAll('.etape')
+  },
+  methods: {
+    checkStep (e) {
+      // let allSteps = document.querySelectorAll('.etape')
+      let parentBtn = e.target.closest('.etape')
+      let parentBtnIsActive = e.target.closest('.etape').classList.contains('active')
+      let parentNextEl = e.target.closest('.etape').nextElementSibling
+
+      if (parentBtnIsActive) {
+        parentNextEl.classList.add('active')
+        parentNextEl.classList.remove('hidden')
+        
+        parentBtn.classList.remove('active')
+        parentBtn.classList.add('hidden')
+
+        this.steps.forEach((step, index) => {
+          if(step.classList.contains('active')) {
+            this.indexActive = index
+          }
+        })
+      }
+    },
+    previousStep (e) {
+      // let allSteps = document.querySelectorAll('.etape')
+      let parentBtn = e.target.closest('.etape')
+      let parentBtnIsActive = e.target.closest('.etape').classList.contains('active')
+      let parentNextEl = e.target.closest('.etape').previousElementSibling
+
+      if (parentBtnIsActive) {
+        parentNextEl.classList.add('active')
+        parentNextEl.classList.remove('hidden')
+        
+        parentBtn.classList.remove('active')
+        parentBtn.classList.add('hidden')
+
+        this.steps.forEach((step, index) => {
+          if(step.classList.contains('active')) {
+            this.indexActive = index
+          }
+        })
+      }
+    }
   }
 }
 </script>
+
+<style lang="scss">
+$primary : #2d2f39;
+$secondary : #373945;
+$blue-link : #0175fb;
+$blue-btn : #0076ff;
+
+.home {
+  min-height: 100vh;
+  background-color: $primary;
+}
+
+.etape {
+  padding: 30px;
+  background-color: $secondary;
+  
+  &.hidden {
+    display: none;
+  }
+  label {
+    display: block;
+    margin-top: 20px;
+    margin-bottom: 10px;
+    text-align: left;
+  }
+
+  label, input, select {
+    width: 100%;
+  }
+
+  input, select {
+    height: 35px;
+    padding-left: 10px;
+    color: #fff;
+    border: 1px solid #fff;
+    border-radius: 5px;
+    background-color: transparent;
+    box-sizing: border-box;
+
+    &::placeholder {
+      color: #ccc;
+    }
+  }
+
+  select {
+    option {
+      color: #000;
+    }
+  }
+}
+
+.etape-double {
+  display: flex;
+
+  .block1 {
+    width: 33%;
+  }
+
+  .block2 {
+    width: calc(67% - 11px);
+    margin-left: 11px;
+  }
+}
+
+// Btns prev, next
+.btn-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 14px;
+
+  .btn-previous, .btn-next {
+    transition: 1s;
+    &:hover {
+      opacity: .7;
+    }
+  }
+
+  .btn-previous {
+    color: $blue-link;
+    text-transform: uppercase;
+    cursor: pointer;
+  }
+
+  .btn-next {
+    padding: 10px;
+    background-color: $blue-btn;
+    text-transform: uppercase;
+    color: #fff;
+    text-transform: uppercase;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+}
+
+.required-form-info {
+  font-size: 12px;
+  font-style: italic;
+  text-align: left;
+}
+
+.form-container {
+  max-width: 500px;
+  margin: 0 auto;
+}
+</style>
